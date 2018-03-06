9efc5e80e1b345b075f98fcd2e60fa35fd52438a
RavikAnika <ravanika@in.ibm.com>
2018-03-07 00:26:25 +0530

    Commit for test cases

---
+++ ISS-PassesTests/PassesTests.swift
@@ -0,0 +1,45 @@
+//
+//  PassesTests.swift
+//  ISS-Passes
+//
+//  Created by Ravi on 07/03/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import XCTest
+
+
+class PassesTests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testgetPasses() {
+        Pass.getAllPasses(Lat: 12.9716, long: 77.5946) { (success, error) in
+            XCTAssertNotNil(error,"webservice is getting nil value")
+            XCTAssertTrue(success)
+
+        }
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
